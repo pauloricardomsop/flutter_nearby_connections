93a421c79f8f7cdeb90eab8e4d1828bc5750242c
Paulo Ricardo <pauloricardomsop@gmail.com>
2023-03-08 15:20:41 -0300

    update

---
+++ android/app/src/main/kotlin/com/example/flutter_nearby_connections_plus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_nearby_connections_plus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Classes/FlutterNearbyConnectionsPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterNearbyConnectionsPlugin : NSObject<FlutterPlugin>
-@end
